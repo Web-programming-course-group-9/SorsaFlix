4711fb37c109c9a53cd2531266a5fc4b146b7f2f
Elmo <elehtosa24@students.oamk.fi>
2026-09-13 15:14:05 +0300

    rekisteröitymislomake fronttiin ja brypt

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,10 @@
+import { Link } from "react-router-dom";
+
 function Login() {
   return (
     <main className="page">
       <h1>Log in</h1>
+      <Link to="/register">Ei tiliä? Rekisteröidy</Link>
     </main>
   );
 }
